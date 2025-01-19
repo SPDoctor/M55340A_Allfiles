9a26fb2c0a1c0c63e0a329769f9ca5040679121c
SURFACE7PLUS\BillA <BillA@flosim.com>
2025-01-19 14:51:24 +0000

    Fixed lab 6

--- Mod06/Labfiles/01_ButterfliesShop_begin/Models/Butterfly.cs
+++ Mod06/Labfiles/01_ButterfliesShop_begin/Models/Butterfly.cs
@@ -2,5 +2,15 @@
 {
     public class Butterfly
     {
+        public int Id { get; set; }
+        public string CommonName { get; set; }
+        public Family? ButterflyFamily { get; set; }
+        public int? Quantity { get; set; }
+        public string Characteristics { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public IFormFile PhotoAvatar { get; set; }
+        public string ImageName { get; set; }
+        public byte[] PhotoFile { get; set; }
+        public string ImageMimeType { get; set; }
     }
 }
