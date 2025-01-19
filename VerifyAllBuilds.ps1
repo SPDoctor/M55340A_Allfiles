67b03d440edf08fca8ff0dac72c5eb44ee75ec70
SURFACE7PLUS\BillA <BillA@flosim.com>
2025-01-19 11:48:11 +0000

    Fixed build error in Mod1 democode WebApplication1

--- VerifyAllBuilds.ps1
+++ VerifyAllBuilds.ps1
@@ -2,8 +2,11 @@ param (
     [string]$rootPath = "."
 )
 
-# Function to build a project or solution
-function Build-ProjectOrSolution {
+$startPath = Get-Location
+$global:failedCount = 0
+
+# Function to build a project
+function Build-Project {
     param (
         [string]$path
     )
@@ -15,27 +18,31 @@ function Build-ProjectOrSolution {
     Set-Location -Path $directory
 
     # Restore the project dependencies
-    #dotnet restore $path
+    dotnet restore $path
 
     # Build the project
-    #$buildResult = dotnet build $path
+    dotnet build $path
 
     # Check if the build was successful
     if ($LASTEXITCODE -eq 0) {
         Write-Host "Build succeeded for $path."
     } else {
         Write-Host "Build failed for $path."
-        exit 1
+        $global:failedCount++
     }
 }
 
-# Get all .csproj and .sln files in the subdirectories
-$projectFiles = Get-ChildItem -Path $rootPath -Recurse -Include *.csproj, *.sln
+# Get all .csproj files in the subdirectories
+$projectFiles = Get-ChildItem -Path $rootPath -Recurse -Include *.csproj
 
 # Build each project or solution
 foreach ($projectFile in $projectFiles) {
-    Build-ProjectOrSolution -path $projectFile.FullName
+    # call Build-ProjectOrSolution function and save return value
+    Build-Project -path $projectFile.FullName
 }
 
-Write-Host "All builds completed."
+if($global:failedCount -gt 0) { Write-Host "All builds completed but $failedCount failed." }
+else { Write-Host "All builds completed successfully." }
+Set-Location -Path $startPath
+
 exit 0
