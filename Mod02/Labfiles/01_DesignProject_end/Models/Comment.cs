413ebc9e6f06a68fd19fa706a2c8f01cf43f37cc
SURFACE7PLUS\BillA <BillA@flosim.com>
2025-01-20 15:49:52 +0000

    nullable variables fixes

--- Mod02/Labfiles/01_DesignProject_end/Models/Comment.cs
+++ Mod02/Labfiles/01_DesignProject_end/Models/Comment.cs
@@ -4,9 +4,9 @@
     {
         public int CommentID { get; set; }
         public int User { get; set; }
-        public string Owner { get; set; }
-        public string Subject { get; set; }
-        public string Body { get; set; }
+        public string? Owner { get; set; }
+        public string? Subject { get; set; }
+        public string? Body { get; set; }
         public int PhotoID { get; set; }
     }
 }
--- Mod02/Labfiles/01_DesignProject_end/Models/Photo.cs
+++ Mod02/Labfiles/01_DesignProject_end/Models/Photo.cs
@@ -4,12 +4,12 @@ namespace DesignProject.Models
 {
     public class Photo
     {
-        public int PhotoID { get; set; }
-        public string Title { get; set; }
-        public byte[] PhotoFile { get; set; }
-        public string Description { get; set; }
+        public int? PhotoID { get; set; }
+        public string? Title { get; set; }
+        public byte[]? PhotoFile { get; set; }
+        public string? Description { get; set; }
         public DateTime CreatedDate { get; set; }
-        public string Owner { get; set; }
-        public List<Comment> Comments { get; set; }
+        public string? Owner { get; set; }
+        public List<Comment>? Comments { get; set; }
     }
 }
